5ad12ad8f361c2fbe1d1a7404f7cf5d5d0749089
Leonardo Sanches <sleonardogabriel@gmail.com>
2024-09-03 14:37:05 -0300

    Add new whatsapp floating button

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+import whatsappLogo from '@/assets/whatsapp.svg'
 import Home from "./components/ui/Home"
 
 export default function App() {
   return (
-    <Home />
+    <>
+      <Home />
+      <a
+        href="https://api.whatsapp.com/send/?phone=5515997681165&text=Ol%C3%A1%21+Tenho+algumas+d%C3%BAvidas+sobre+meus+direitos+trabalhistas.+Voc%C3%AA+poderia+me+ajudar%3F&type=phone_number&app_absent=0"
+        target='_blank'
+        className="fixed z-50 bottom-6 right-6 bg-green-600 w-auto p-2 rounded text-white flex items-center gap-3"
+      >
+        <img className='w-8' src={whatsappLogo} />
+        Whatsapp
+      </a>
+    </>
   )
 }
