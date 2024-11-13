9129b2c58bdc3f195cf463b9c6668df558c9f9ab
Leonardo Sanches <sleonardogabriel@gmail.com>
2024-11-13 11:25:28 -0300

    feat: new articles page and fix images

--- src/components/ui/Home/articles-section.tsx
+++ src/components/ui/Home/articles-section.tsx
@@ -1,23 +1,28 @@
-
-import background from '@/assets/articles-background.jpg'
 import articles from '@/articles.json'
-import passa from '@/assets/passarinho.jpg'
 
 export default function ArticlesSection() {
     return (
-        <section className="h-screen bg-[#101b27] text-white flex flex-col items-center justify-center">
-            <img src={background} alt="" className='absolute opacity-10 object-cover min-h-full min-w-full' />
+        <section className="h-2/3 bg-[#174472] text-white flex flex-col items-center justify-center">
             <div className='px-32 py-16 z-50 gap-20 flex flex-col'>
                 <h1 className="text-4xl text-center">Conheça nossos artigos</h1>
 
-                <div className="flex gap-16 items-center justify-center">
-                    {articles.highlighted.map(article => (
-                        <div className="flex flex-col items-center text-center gap-10 bg-[#174472]">
-                            <img src={passa} alt="" className='w-64 h-48' />
-                            <p>{article.title}</p>
+                <div className="flex justify-between gap-4 px-6 max-w-6xl">
+                    {articles.highlighted.map((article, index) => (
+                        <div
+                            key={index}
+                            className="flex flex-col items-center text-center gap-4 w-1/3 bg-[#d9d9d9] border border-[#d9d9d9] rounded-lg p-6 hover:scale-105 hover:shadow-lg transition-transform duration-300 cursor-pointer"
+                        >
+                            <img
+                                src={`/assets/${article.image}`}
+                                alt={`Imagem do artigo ${index + 1}`}
+                                className="w-full h-40 object-cover rounded-t-md"
+                            />
+                            <h1 className="text-xl font-bold text-gray-800">{article.title}</h1>
+                            <p className="text-gray-600 text-sm">{article.excerpt}</p>
                         </div>
                     ))}
                 </div>
+
             </div>
         </section>
     )
