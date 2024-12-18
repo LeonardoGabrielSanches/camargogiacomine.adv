5bc8eb80dabeb8911568ec5ccd74a835490c53e8
Leonardo Sanches <sleonardogabriel@gmail.com>
2024-12-18 10:21:40 -0300

    add skeleton loading

--- src/components/ui/Articles/index.tsx
+++ src/components/ui/Articles/index.tsx
@@ -4,6 +4,8 @@ import { request, gql } from "graphql-request";
 import { useQuery } from "react-query";
 
 import Header from "@/components/core/Header";
+import Skeleton from "react-loading-skeleton";
+import 'react-loading-skeleton/dist/skeleton.css'
 
 const query = gql`
 query GetArticles($slug: String!) {
@@ -19,11 +21,10 @@ query GetArticles($slug: String!) {
 }
 `
 
-function getPageTitle(error: unknown, isLoading: boolean, articles: [{ titulo: string }]) {
+function getPageTitle(error: unknown, articles: [{ titulo: string }]) {
     return error || articles.length <= 0 ?
-        (<h1 className="text-4xl font-bold text-[#174472] text-center">Artigo não encontrado</h1>) : isLoading ?
-            (<h1 className="text-4xl font-bold text-[#174472] text-center">Carregando...</h1>)
-            : <h1 className="text-4xl font-bold text-[#174472] text-center">{articles[0].titulo}</h1>
+        (<h1 className="text-4xl font-bold text-[#174472] text-center">Artigo não encontrado</h1>)
+        : <h1 className="text-4xl font-bold text-[#174472] text-center">{articles[0].titulo}</h1>
 }
 
 export function Articles() {
@@ -45,25 +46,31 @@ export function Articles() {
         enabled: !!params.slug,
     });
 
-    if (!data)
-        return <h1>Erro na aplicação</h1>
-
     return (
         <section id="inicio" className="min-h-screen bg-gradient-to-b from-[#174472] to-[#d9d9d9] p-6 lg:p-16 text-[#1f364e]">
             <Header mainPage={false} />
             <div className="max-w-5xl mx-auto mt-12 space-y-16">
                 <div className="bg-white p-10 rounded-xl shadow-2xl space-y-8">
+                    {isLoading ? (
+                        <div>
+                            <Skeleton height={120} />
+                            <Skeleton height={240} />
+                            <Skeleton count={20} />
+                        </div>
+                    ) : (
+                        <>
+                            {getPageTitle(error, data?.articles ?? [{ titulo: "Artigo não encontrado" }])}
 
-                    {getPageTitle(error, isLoading, data.articles)}
-
-                    <div className="space-y-8">
-                        <img
-                            src={data.articles[0].imagem.url}
-                            alt={data.articles[0].titulo}
-                            className="w-full max-w-md mx-auto rounded-lg shadow-lg object-cover"
-                        />
-                        <div id="conteudo-artigo" className="space-y-6" dangerouslySetInnerHTML={{ __html: data.articles[0].conteudo.html }} />
-                    </div>
+                            <div className="space-y-8">
+                                <img
+                                    src={data?.articles[0].imagem.url}
+                                    alt={data?.articles[0].titulo}
+                                    className="w-full max-w-md mx-auto rounded-lg shadow-lg object-cover"
+                                />
+                                <div id="conteudo-artigo" className="space-y-6" dangerouslySetInnerHTML={{ __html: data?.articles[0].conteudo.html ?? '' }} />
+                            </div>
+                        </>
+                    )}
                 </div>
             </div>
         </section>
