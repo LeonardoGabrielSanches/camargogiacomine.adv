c634c7d8ed5bdac1833366922d3ddf74b4abf902
Leonardo Sanches <sleonardogabriel@gmail.com>
2025-02-04 15:54:06 -0300

    only one article for mobile

--- src/components/ui/Home/articles-section.tsx
+++ src/components/ui/Home/articles-section.tsx
@@ -1,7 +1,7 @@
 import { fetchHygraphData } from '@/lib/graphql';
-import { gql } from 'graphql-request'
+import { gql } from 'graphql-request';
 import { useQuery } from 'react-query';
-import { Link } from 'react-router'
+import { Link } from 'react-router';
 
 const query = gql`
 query GetArticles {
@@ -14,10 +14,10 @@ query GetArticles {
     slug
   }
 }
-`
+`;
 
 export default function ArticlesSection() {
-    const { data } = useQuery<{ articles: [{ titulo: string, imagem: { url: string }, excerto: string, slug: string }] }>({
+    const { data } = useQuery<{ articles: { titulo: string, imagem: { url: string }, excerto: string, slug: string }[] }>({
         queryKey: ['articles'],
         queryFn: async () => await fetchHygraphData(query),
     });
@@ -28,21 +28,42 @@ export default function ArticlesSection() {
                 <h1 className="text-2xl lg:text-4xl text-center">Conheça nossos artigos</h1>
 
                 <div className="flex flex-wrap justify-center gap-6 lg:gap-12 w-full">
-                    {data?.articles.map((article, index) => (
-                        <Link
-                            key={index}
-                            to={`/artigo/${article.slug}`}
-                            className="flex flex-col items-center text-center gap-4 w-full sm:w-[48%] lg:w-[30%] bg-[#d9d9d9] border border-[#d9d9d9] rounded-lg p-6 hover:scale-105 hover:shadow-lg transition-transform duration-300 cursor-pointer"
-                        >
-                            <img
-                                src={article.imagem.url}
-                                alt={article.titulo}
-                                className="w-full h-40 object-cover rounded-t-md"
-                            />
-                            <h1 className="text-lg lg:text-xl font-bold text-gray-800">{article.titulo}</h1>
-                            <p className="text-gray-600 text-sm">{article.excerto}</p>
-                        </Link>
-                    ))}
+                    {data?.articles
+                        .slice(0, 1)
+                        .map((article, index) => (
+                            <Link
+                                key={index}
+                                to={`/artigo/${article.slug}`}
+                                className="flex flex-col items-center text-center gap-4 w-full sm:w-[48%] lg:w-[30%] bg-[#d9d9d9] border border-[#d9d9d9] rounded-lg p-6 hover:scale-105 hover:shadow-lg transition-transform duration-300 cursor-pointer lg:flex"
+                            >
+                                <img
+                                    src={article.imagem.url}
+                                    alt={article.titulo}
+                                    className="w-full h-40 object-cover rounded-t-md"
+                                />
+                                <h1 className="text-lg lg:text-xl font-bold text-gray-800">{article.titulo}</h1>
+                                <p className="text-gray-600 text-sm">{article.excerto}</p>
+                            </Link>
+                        ))}
+
+
+                    {data?.articles
+                        .slice(1)
+                        .map((article, index) => (
+                            <Link
+                                key={index}
+                                to={`/artigo/${article.slug}`}
+                                className="hidden lg:flex flex-col items-center text-center gap-4 w-full sm:w-[48%] lg:w-[30%] bg-[#d9d9d9] border border-[#d9d9d9] rounded-lg p-6 hover:scale-105 hover:shadow-lg transition-transform duration-300 cursor-pointer"
+                            >
+                                <img
+                                    src={article.imagem.url}
+                                    alt={article.titulo}
+                                    className="w-full h-40 object-cover rounded-t-md"
+                                />
+                                <h1 className="text-lg lg:text-xl font-bold text-gray-800">{article.titulo}</h1>
+                                <p className="text-gray-600 text-sm">{article.excerto}</p>
+                            </Link>
+                        ))}
                 </div>
 
                 <div className="flex justify-center mt-4">
@@ -55,5 +76,5 @@ export default function ArticlesSection() {
                 </div>
             </div>
         </section>
-    )
+    );
 }
