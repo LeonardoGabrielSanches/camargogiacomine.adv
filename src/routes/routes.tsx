cc661d16803f2b6f3bd3e737de26fd5959bf7920
Leonardo Sanches <sleonardogabriel@gmail.com>
2025-02-14 10:36:31 -0300

    fix route

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,6 +1,6 @@
 import { Article } from "@/components/ui/Article"
 import { Articles } from "@/components/ui/Articles"
-import BPCSection from "@/components/ui/BcpLoas"
+import BPCSection from "@/components/ui/BpcLoas"
 import { Home } from "@/components/ui/Home"
 import { WorkingAreasPage } from "@/components/ui/WorkingAreas"
 import { BrowserRouter, Route, Routes } from "react-router"
@@ -14,7 +14,7 @@ export function AppRoutes() {
                 <Route path="areas-atuacao/:slug" element={<WorkingAreasPage />} />
                 <Route path="artigos" element={<Articles />} />
                 <Route path="artigo/:slug" element={<Article />} />
-                <Route path="bcploas" element={<BPCSection />} />
+                <Route path="bpcloas" element={<BPCSection />} />
             </Routes>
         </BrowserRouter>
     )
